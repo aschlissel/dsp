3943fd21dfd16e13dd80221401e7a0262f04b71d
aschlissel <apschlissel@gmail.com>
2016-06-08 14:52:42 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,21 @@
-PLACE YOUR CODE HERE
+#Q1.
+def different_degrees(diff):
+    #create two lists to perform operations
+    diff_degrees = []
+    new_diff_degrees = []
+
+    #append faculty column to list
+    [diff_degrees.append(col[1]) for col in csv_f]
+
+    #remove column heading
+    del diff_degrees[0]
+
+    #remove the periods in degrees to make degrees uniform
+    diff_degrees = map(lambda diff_degrees: diff_degrees.replace(".", ""), diff_degrees)
+
+    #separate multiple-degree strings in that list
+    [new_diff_degrees.extend(item.split()) for item in diff_degrees]
+
+    #count the number of degrees in each category
+    diff_degrees_count = collections.Counter(new_diff_degrees)
+    return diff_degrees_count
