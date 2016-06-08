c6bc5e73cc66efc4ac0ebce67bb6cea55834680a
aschlissel <apschlissel@gmail.com>
2016-06-08 18:48:16 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,19 @@
-PLACE YOUR CODE HERE
+import csv
+
+f = open('faculty.csv')
+csv_f = csv.reader(f)
+
+def emails(csv):
+    email = []
+    [email.append(col[3]) for col in csv_f]
+    del email[0]
+    return email
+
+emails_out = open("emails.csv", "w")
+emails_writer = csv.writer(emails_out)
+data = emails(csv_f)
+
+for item in data:
+    emails_writer.writerow([item])
+
+emails_out.close()
