bb37edecbce18b687443a6529db1477d36878fcd
aschlissel <apschlissel@gmail.com>
2016-06-08 15:29:54 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,5 +1,11 @@
+import csv
+import collections
+
+f = open('faculty.csv')
+csv_f = csv.reader(f)
+
 #Q1.
-def different_degrees(diff):
+def different_degrees(csv_f):
     #create two lists to perform operations
     diff_degrees = []
     new_diff_degrees = []
@@ -19,3 +25,24 @@ def different_degrees(diff):
     #count the number of degrees in each category
     diff_degrees_count = collections.Counter(new_diff_degrees)
     return diff_degrees_count
+
+
+#Q2.
+
+def titles(csv_f):
+
+    #create list to perform operations
+    prof_titles = []
+
+    #append titles column to list
+    [prof_titles.append(col[2]) for col in csv_f]
+
+    #remove column heading
+    del prof_titles[0]
+
+    #change the typo 'is' to 'of'
+    prof_titles = map(lambda prof_titles: prof_titles.replace("is ", "of "), prof_titles)
+
+    #count title frequencies
+    prof_titles_count = collections.Counter(prof_titles)
+    return prof_titles_count
