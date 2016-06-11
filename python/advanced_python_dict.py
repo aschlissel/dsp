76074cbb428eef29686e025bc27ab3fb1a97e665
aschlissel <apschlissel@gmail.com>
2016-06-10 21:20:28 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,27 @@
-PLACE YOUR CODE HERE
+#read csv using pandas, skip spaces in header
+import pandas as pd
+csv_pd = pd.read_csv('faculty.csv', skipinitialspace=True)
+
+df = pd.DataFrame(csv_pd)
+
+#Q6
+
+#create a new column for last names
+df['last_names'] = [item.split(' ')[-1] for item in df['name']]
+last_names = df['last_names']
+
+names = df['name']
+
+#create dictionary
+q6_name_dict = {}
+for this_name in names:
+    x = df[names == this_name]
+    for index, row in x.iterrows():
+        q6_name_dict.setdefault(row['last_names'], []).append(row[['degree', 'title', 'email']].tolist())
+
+for key in q6_name_dict.keys()[:3]:
+    print key, q6_name_dict[key]
+    
+    
+#Q7
+
